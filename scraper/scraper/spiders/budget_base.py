42f355d3330759030ed8c586d459a1e61e02cae7
shreyaagrawal0809 <shreyaagrawal0809@gmail.com>
2019-07-01 14:21:03 +0530

    add base scrapper for budget

---
+++ scraper/scraper/spiders/budget_base.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+import pdb
+
+class BudgetbasespiderSpider(scrapy.Spider):
+    name = 'budget_crawler'
+    allowed_domains = ['ebudget.hp.nic.in']
+    start_urls = ['https://ebudget.hp.nic.in/BudHome.aspx/']
+    
+    def parse(self, response):
+        result = response.css('#tab-content1 ul li:nth-of-type(2) p.pa a::attr(onclick)').extract()
+        filename = response.css('#tab-content1 ul li:nth-of-type(2) p.pa a::text').extract()
+      
+        print(result)
+        for i in range(len(result)):
+            
+            pdf_url = re.findall(".*\('(.*)'\)", result[i])
+            pdf_name = filename[i]
+            yield response.follow(pdf_url[0], self.save_pdf, meta={'pdf_name': pdf_name})
+            
+            
+
+           
+	    
+    def save_pdf(self, response):
+        pdf_names = '../../datasets/budget' + response.meta['pdf_name'] + '.pdf'
+        file = open(pdf_names, 'wb')
+        file.write(response.body)
+        file.close()
+    
+       
+
+
+       
+       
+       
