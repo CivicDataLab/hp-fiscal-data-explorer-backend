a0df6f3361972bc6262039ec4b7977cb2ec48546
shreyaagrawal0809 <shreyaagrawal0809@gmail.com>
2019-07-02 15:30:11 +0530

    modified crawler with type_budget variable for both expenditure and receipts

--- scraper/scraper/spiders/budget_base.py
+++ scraper/scraper/spiders/budget_base.py
@@ -7,6 +7,7 @@ class BudgetbasespiderSpider(scrapy.Spider):
     name = 'budget_crawler'
     allowed_domains = ['ebudget.hp.nic.in']
     start_urls = ['https://ebudget.hp.nic.in/BudHome.aspx/']
+
     
     def parse(self, response):
         result = response.css('#tab-content1 ul li:nth-of-type(2) p.pa a::attr(onclick)').extract()
@@ -19,27 +20,36 @@ class BudgetbasespiderSpider(scrapy.Spider):
             
             pdf_url = re.findall(".*\('(.*)'\)", result[i])
             pdf_name = filename[i]  
-            yield response.follow(pdf_url[0], self.save_pdf, meta={'pdf_name': pdf_name})
+            type_budget = 'expenditure'
+            print(pdf_name,pdf_url)
+            yield response.follow(pdf_url[0], self.save_pdf, meta={'pdf_name': pdf_name, 'type_budget': type_budget})
         
         for i in range(len(result_receipts)):
 
             pdf_url_receipts = re.findall(".*\('(.*)'\)", result_receipts[i])
             pdf_name_receipts  = filename_receipts[i]  
-            yield response.follow(pdf_url_receipts[0], self.save_pdf_receipts, meta={'pdf_name_receipts': pdf_name_receipts})
-               
+            type_budget = 'receipts'
+            yield response.follow(pdf_url_receipts[0], self.save_pdf, meta={'pdf_name_receipts': pdf_name_receipts, 'type_budget':type_budget})
+            
     	    
     def save_pdf(self, response):
-        pdf_names = '../../datasets/budget/' + response.meta['pdf_name'] + '.pdf'
-        file = open(pdf_names, 'wb')
-        file.write(response.body)
-        file.close()
+        if response.meta['type_budget'] == 'expenditure':
+            pdf_names = '../../datasets/budget/' + response.meta['pdf_name'] + '.pdf'
+            file = open(pdf_names, 'wb')
+            file.write(response.body)
+            file.close()
+        else:
+            pdf_names = '../../datasets/receipts/' + response.meta['pdf_name_receipts'] + '.pdf'
+            file = open(pdf_names, 'wb')
+            file.write(response.body)
+            file.close()
 
 
-    def save_pdf_receipts(self, response):
-        pdf_names = '../../datasets/receipts/' + response.meta['pdf_name_receipts'] + '.pdf'
-        file = open(pdf_names, 'wb')
-        file.write(response.body)
-        file.close()
+ #   def save_pdf_receipts(self, response):
+  #      pdf_names = '../../datasets/receipts/' + response.meta['pdf_name_receipts'] + '.pdf'
+   #     file = open(pdf_names, 'wb')
+    #    file.write(response.body)
+     #   file.close()
     
     
  
