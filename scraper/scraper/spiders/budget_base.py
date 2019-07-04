cc297b7466de992a666c7b21b99ce02a1ecac56a
shreyaagrawal0809 <shreyaagrawal0809@gmail.com>
2019-07-04 12:45:39 +0530

    add scrapper for budget expenditure machine readable datasets

--- scraper/scraper/spiders/budget_base.py
+++ scraper/scraper/spiders/budget_base.py
@@ -1,59 +1,156 @@
 # -*- coding: utf-8 -*-
-import scrapy
-import re
+import csv
+import os
+from urllib.parse import urlencode
 import pdb
+import scrapy
+from scrapy.exceptions import CloseSpider
+from scrapy.spidermiddlewares.httperror import HttpError
 
-class BudgetbasespiderSpider(scrapy.Spider):
-    name = 'budget_crawler'
-    allowed_domains = ['ebudget.hp.nic.in']
-    start_urls = ['https://ebudget.hp.nic.in/BudHome.aspx/']
-
-    
-    def parse(self, response):
-        result = response.css('#tab-content1 ul li:nth-of-type(2) p.pa a::attr(onclick)').extract()
-        filename = response.css('#tab-content1 ul li:nth-of-type(2) p.pa a::text').extract()
-        result_receipts = response.css('#tab-content ul li:nth-of-type(8) p.pa a::attr(onclick)').extract()
-        filename_receipts = response.css('#tab-content ul li:nth-of-type(8) p.pa a::text').extract()
+from scraper.settings import DATASET_PATH
+from scraper.utils import parsing_utils
 
 
-        for i in range(len(result)):
-            
-            pdf_url = re.findall(".*\('(.*)'\)", result[i])
-            pdf_name = filename[i]  
-            type_budget = 'expenditure'
-            print(pdf_name,pdf_url)
-            yield response.follow(pdf_url[0], self.save_pdf, meta={'pdf_name': pdf_name, 'type_budget': type_budget})
-        
-        for i in range(len(result_receipts)):
+class BudgetBaseSpider(scrapy.Spider):
+    '''
+    Base spider for HP budget. It has methods for making dataset requests
+    for a given HOD code with a given query with given date.
+    '''
+    allowed_domains = ['himkosh.hp.nic.in']
 
-            pdf_url_receipts = re.findall(".*\('(.*)'\)", result_receipts[i])
-            pdf_name_receipts  = filename_receipts[i]  
-            type_budget = 'receipts'
-            yield response.follow(pdf_url_receipts[0], self.save_pdf, meta={'pdf_name_receipts': pdf_name_receipts, 'type_budget':type_budget})
-            
-    	    
-    def save_pdf(self, response):
-        if response.meta['type_budget'] == 'expenditure':
-            pdf_names = '../../datasets/budget/' + response.meta['pdf_name'] + '.pdf'
-            file = open(pdf_names, 'wb')
-            file.write(response.body)
-            file.close()
+    def __init__(self, *args, **kwargs):
+        super(BudgetBaseSpider, self).__init__(*args, **kwargs)
+        if not hasattr(self, 'date'):
+            raise CloseSpider('No date given!')
+
+    def make_dataset_request(self, params):
+        '''
+        Construct and yield a scrapy request for a dataset.
+        '''
+        # generate a filepath to store the dataset in.
+        filename = '{}_{}.csv'.format(self.name, self.date)
+
+        filepath = os.path.join(DATASET_PATH, filename)
+
+        # don't request the same dataset again if it's already collected previously
+        # check if a file with a particular dataset name exist, if it does then
+        # also check if it's empty or not, if it's empty we request it again.
+        if not os.path.exists(filepath) or not os.stat(filepath).st_size:
+            query_params = {
+                'from_date': params['date'],  # format: yyyymmdd
+                'To_date': params['date'],      # format: yyyymmdd
+                'ddlquery': params['query_id'],
+                'HODCode': params['hod_name'],
+                'Str': params['query_name'],
+                'Unit' : params['unit']
+            }
+
+            yield scrapy.Request(
+                self.query_url.format(urlencode(query_params)), # pylint: disable=no-member
+                self.parse_dataset,
+                errback=self.handle_err, meta={'filepath': filepath}
+            )
+
+    def start_requests(self):
+        '''
+        This method is called when the spider opens.
+        It will check if arguments for specific query, HOD code were provided.
+        If they were provided then it'll query specifically for that otherwise it goes to
+        the expenditures' home page and collects for all the treasuries.
+        '''
+        if (
+                not hasattr(self, 'query_id')
+                and not hasattr(self, 'query_name')
+                and not hasattr(self, 'hod_code')
+            ):
+            yield scrapy.Request(self.start_urls[0], self.parse)
         else:
-            pdf_names = '../../datasets/receipts/' + response.meta['pdf_name_receipts'] + '.pdf'
-            file = open(pdf_names, 'wb')
-            file.write(response.body)
-            file.close()
+            for ddo_code in self.get_ddo_codes(self.treasury_id): #pylint: disable=no-member
+                params = {
+                    'date': self.date,  #pylint: disable=no-member
+                    'query_id': self.query_id,  #pylint: disable=no-member
+                    'query_name': self.query_name,  #pylint: disable=no-member
+                    'hod_code': self.hod_code
+                }
+                yield self.make_dataset_request(params)
 
+    def parse(self, response):
+        '''
+        Collect queryable params and make dataset queries.
+        Parameters to be collected are:
 
- #   def save_pdf_receipts(self, response):
-  #      pdf_names = '../../datasets/receipts/' + response.meta['pdf_name_receipts'] + '.pdf'
-   #     file = open(pdf_names, 'wb')
-    #    file.write(response.body)
-     #   file.close()
-    
-    
- 
+        query_id,
+        HODCode,
+        query_text
+        '''
+        # collect details of query 9 that gives consolidated data.
+        query_elem = response.xpath('id("ddlQuery")/option')[self.query_index]  # pylint: disable=no-member
 
-       
-       
-       
+        # extract parameters for query i.e. query id and its text.
+        query_id = query_elem.xpath('./@value').extract_first()
+        query_name = query_elem.xpath('.//text()').extract_first()
+
+        # remove extra whitespaces from query text.
+        query_name = parsing_utils.clean_text(query_name)
+
+        # extract parameters for hod_code i.e.its text.
+        hod_elem = response.xpath('id("cmbDpt")/option')[0]
+
+        hod_name = hod_elem.xpath('.//@value').extract_first()
+
+        # remove extra whitespaces from query text.
+        hod_name = parsing_utils.clean_text(hod_name)
+
+        params = {
+            'date': self.date,  #pylint: disable=no-member
+            'query_id': query_id,
+            'query_name': query_name,
+            'hod_name': hod_name,
+            'unit': self.unit
+            }
+        return self.make_dataset_request(params)
+
+    def parse_dataset(self, response):  #pylint: disable=no-self-use
+        '''
+        Parse each dataset page to collect the data in a csv file.
+        output: a csv file named with budget_expenditures_date format.
+        '''
+        # header row for the file.
+        heads = response.xpath('//table//tr[@class="popupheadingeKosh"]//td//text()').extract()
+        pdb.set_trace()
+        # all other rows
+        data_rows = response.xpath('//table//tr[contains(@class, "pope")]')
+
+        if not data_rows:
+            return
+
+        # prepare file name and its path to write the file.
+        filepath = response.meta.get('filepath')
+
+        with open(filepath, 'w') as output_file:
+            writer = csv.writer(output_file, delimiter=',')
+
+            # write the header
+            writer.writerow(heads)
+
+            # write all other rows
+            for row in data_rows:
+                cols = row.xpath('.//td')
+                observation = []
+                for col in cols:
+                    # since we need consistency in the row length,
+                    # we need to extract each cell and set empty string when no data found.
+                    # by default scrapy omits the cell if it's empty and it can cause inconsistent row lengths.  # pylint:disable=line-too-long
+                    observation.append(col.xpath('./text()').extract_first(' '))
+                writer.writerow(observation)
+
+    def handle_err(self, failure):
+        '''
+        Logs the request and response details when a request fails.
+        '''
+        if failure.check(HttpError):
+            response = failure.value.response
+            request = response.request
+            self.logger.error('Request: {}'.format(request))
+            self.logger.error('Request headers: {}'.format(request.headers))
+            self.logger.error('Response headers: {}'.format(response.headers))
---
+++ scraper/scraper/spiders/budget_base_pdf.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import re
+import pdb
+
+class BudgetbasespiderSpider(scrapy.Spider):
+    name = 'budget_crawler'
+    allowed_domains = ['ebudget.hp.nic.in']
+    start_urls = ['https://ebudget.hp.nic.in/BudHome.aspx/']
+
+    
+    def parse(self, response):
+        result = response.css('#tab-content1 ul li:nth-of-type(2) p.pa a::attr(onclick)').extract()
+        filename = response.css('#tab-content1 ul li:nth-of-type(2) p.pa a::text').extract()
+        result_receipts = response.css('#tab-content ul li:nth-of-type(8) p.pa a::attr(onclick)').extract()
+        filename_receipts = response.css('#tab-content ul li:nth-of-type(8) p.pa a::text').extract()
+
+
+        for i in range(len(result)):
+            
+            pdf_url = re.findall(".*\('(.*)'\)", result[i])
+            pdf_name = filename[i]  
+            type_budget = 'expenditure'
+            print(pdf_name,pdf_url)
+            yield response.follow(pdf_url[0], self.save_pdf, meta={'pdf_name': pdf_name, 'type_budget': type_budget})
+        
+        for i in range(len(result_receipts)):
+
+            pdf_url_receipts = re.findall(".*\('(.*)'\)", result_receipts[i])
+            pdf_name_receipts  = filename_receipts[i]  
+            type_budget = 'receipts'
+            yield response.follow(pdf_url_receipts[0], self.save_pdf, meta={'pdf_name_receipts': pdf_name_receipts, 'type_budget':type_budget})
+            
+    	    
+    def save_pdf(self, response):
+        if response.meta['type_budget'] == 'expenditure':
+            pdf_names = '../../datasets/budget/' + response.meta['pdf_name'] + '.pdf'
+            file = open(pdf_names, 'wb')
+            file.write(response.body)
+            file.close()
+        else:
+            pdf_names = '../../datasets/receipts/' + response.meta['pdf_name_receipts'] + '.pdf'
+            file = open(pdf_names, 'wb')
+            file.write(response.body)
+            file.close()
+
+
+ #   def save_pdf_receipts(self, response):
+  #      pdf_names = '../../datasets/receipts/' + response.meta['pdf_name_receipts'] + '.pdf'
+   #     file = open(pdf_names, 'wb')
+    #    file.write(response.body)
+     #   file.close()
+    
+    
+ 
+
+       
+       
+       
