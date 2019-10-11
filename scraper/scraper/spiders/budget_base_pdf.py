009c7a93994763858161ae6463dbb8e67e413794
shreyaagrawal0809 <shreyaagrawal0809@gmail.com>
2019-10-11 11:15:55 +0530

    minor format corrections

--- scraper/scraper/spiders/budget_base_pdf.py
+++ scraper/scraper/spiders/budget_base_pdf.py
@@ -7,31 +7,24 @@ class BudgetbasespiderSpider(scrapy.Spider):
     name = 'budget_crawler'
     allowed_domains = ['ebudget.hp.nic.in']
     start_urls = ['https://ebudget.hp.nic.in/BudHome.aspx/']
-
-    
     def parse(self, response):
         result = response.css('#tab-content1 ul li:nth-of-type(2) p.pa a::attr(onclick)').extract()
         filename = response.css('#tab-content1 ul li:nth-of-type(2) p.pa a::text').extract()
-        result_receipts = response.css('#tab-content ul li:nth-of-type(8) p.pa a::attr(onclick)').extract()
+        result_receipts = response.css('#tab-content ul li:nth-of-type(8) p.pa a::attr(onclick)').extract() # pylint: disable=line-too-long
         filename_receipts = response.css('#tab-content ul li:nth-of-type(8) p.pa a::text').extract()
 
 
         for i in range(len(result)):
-            
             pdf_url = re.findall(".*\('(.*)'\)", result[i])
-            pdf_name = filename[i]  
+            pdf_name = filename[i]
             type_budget = 'expenditure'
-            print(pdf_name,pdf_url)
-            yield response.follow(pdf_url[0], self.save_pdf, meta={'pdf_name': pdf_name, 'type_budget': type_budget})
-        
+            print(pdf_name, pdf_url)
+            yield response.follow(pdf_url[0], self.save_pdf, meta={'pdf_name': pdf_name, 'type_budget': type_budget})  # pylint: disable=line-too-long
         for i in range(len(result_receipts)):
-
             pdf_url_receipts = re.findall(".*\('(.*)'\)", result_receipts[i])
-            pdf_name_receipts  = filename_receipts[i]  
+            pdf_name_receipts = filename_receipts[i]
             type_budget = 'receipts'
-            yield response.follow(pdf_url_receipts[0], self.save_pdf, meta={'pdf_name_receipts': pdf_name_receipts, 'type_budget':type_budget})
-            
-    	    
+            yield response.follow(pdf_url_receipts[0], self.save_pdf, meta={'pdf_name_receipts': pdf_name_receipts, 'type_budget':type_budget})  # pylint: disable=line-too-long
     def save_pdf(self, response):
         if response.meta['type_budget'] == 'expenditure':
             pdf_names = '../../datasets/budget/' + response.meta['pdf_name'] + '.pdf'
@@ -41,4 +34,5 @@ class BudgetbasespiderSpider(scrapy.Spider):
             pdf_names = '../../datasets/receipts/' + response.meta['pdf_name_receipts'] + '.pdf'
             file = open(pdf_names, 'wb')
             file.write(response.body)
-            file.close()  
+            file.close()
+            
