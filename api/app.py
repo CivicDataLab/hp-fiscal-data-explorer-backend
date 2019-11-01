a1f78bf44a0f4af10e7e458c99d31834a19b0f42
shreyaagrawal0809 <shreyaagrawal0809@gmail.com>
2019-11-01 16:37:31 +0530

    solve json memory error

--- api/app.py
+++ api/app.py
@@ -7,7 +7,7 @@ import falcon
 from api import budget_ep
 
 # create API
-api = app = falcon.API()
+api = app = falcon.API(middleware=[budget_ep.CORSMiddleware()])
 
 # create endpoints for API.
 api.add_route('/api/detail_exp', budget_ep.DetailExpenditure())
