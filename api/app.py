f878d6dabcaaaa2d44bf2656487f9b1a7911bcfd
shreyaagrawal0809 <shreyaagrawal0809@gmail.com>
2019-11-02 14:44:40 +0530

    remove keys and SOE description from API data structure

--- api/app.py
+++ api/app.py
@@ -12,3 +12,4 @@ api = app = falcon.API(middleware=[budget_ep.CORSMiddleware()])
 # create endpoints for API.
 api.add_route('/api/detail_exp', budget_ep.DetailExpenditure())
 api.add_route('/api/exp_summary', budget_ep.ExpenditureSummary())
+api.add_route('/api/detail_exp_test', budget_ep.DetailExpenditure01())
