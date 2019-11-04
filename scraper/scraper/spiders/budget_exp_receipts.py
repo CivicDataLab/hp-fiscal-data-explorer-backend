a649a00e6fa31cd924706254e4f224faaa0d1be2
curioswati <swati@civicdatalab.in>
2019-11-04 17:07:33 +0530

    add spider for budget receipts data

--- scraper/scraper/spiders/budget_exp_receipts.py
+++ scraper/scraper/spiders/budget_exp_receipts.py
@@ -1,8 +1,14 @@
+'''
+Budget expenditure and receitps spiders
+'''
 # -*- coding: utf-8 -*-
 from scraper.spiders import budget_base
 
 
-class BudgetExpendituresSpider(budget_base.BudgetBaseSpider):
+class BudgetExpendituresSpider(budget_base.ExpenditureBaseSpider):
+    '''
+    BudgetReceiptsSpider
+    '''
     name = 'budget_expenditures'
 
     start_urls = ['http://himkosh.hp.nic.in/treasuryportal/eKosh/ekoshhodAllocationquery.asp']
@@ -13,3 +19,14 @@ class BudgetExpendituresSpider(budget_base.BudgetBaseSpider):
     query_index = 9
 
     unit = '.00001'
+
+class BudgetReceiptsSpider(budget_base.ReceiptBaseSpider):
+    '''
+    BudgetReceiptsSpider
+    '''
+    name = 'budget_receipts'
+
+    # dataset is collected from here.
+    query_url = 'https://himkosh.nic.in/eHPOLTIS/PublicReports/Receipt.asmx/GetReceiptMajorDetail'
+
+    unit = '1'
