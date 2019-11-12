c6ead25dab85b25037bf36a2b12baaac4aa1af03
curioswati <swati@civicdatalab.in>
2019-11-12 19:39:20 +0530

    fix pylint errors

--- api/budget_ep.py
+++ api/budget_ep.py
@@ -3,20 +3,23 @@ budget data endpoints
 '''
 import json
 from datetime import datetime
-import pdb
+
 import falcon
-from io import StringIO
+
 from api.db import CONNECTION
-import pandas as pd
-
-
 
 
 class CORSMiddleware:
+    '''
+    Middleware for handling CORS.
+    '''
     def process_request(self, req, resp):
+        '''
+        process request to set headers.
+        '''
         resp.set_header('Access-Control-Allow-Origin', '*')
 
-        
+
 #NOTE: purposefully ignoring no-member errors
 # Ref https://github.com/falconry/falcon/issues/1553
 def validate_date(req, resp, resource, params):
@@ -70,19 +73,18 @@ class DetailExpenditure():
             record['revised'] = row[15]
 
             response_data['records'].append(record)
-            
-        resp.status = falcon.HTTP_200  #pylint: disable=no-member
-        io = StringIO()
-        data = json.dump(response_data, io)
-        resp.body = io.getvalue()
 
-        
+        resp.status = falcon.HTTP_200  #pylint: disable=no-member
+        resp.body = json.dumps(response_data)
+
 
 class ExpenditureSummary():
     """Expenditure Summary"""
 
     def on_get(self, req, resp):
-
+        '''
+        handle get requests for expenditure summary.
+        '''
         query_string = "select * from himachal_budget_expenditure_summary"  # pylint: disable=line-too-long
         query = CONNECTION.execute(query_string)
         data_rows = query.fetchall()
@@ -100,9 +102,8 @@ class ExpenditureSummary():
 
         resp.status = falcon.HTTP_200  #pylint: disable=no-member
         resp.body = json.dumps(response_data)
-        
 
-    
+
 @falcon.before(validate_date)
 class DetailExpenditure01():
     '''
@@ -110,7 +111,7 @@ class DetailExpenditure01():
     '''
     def on_get(self, req, resp):
         '''
-        method for getting detail expenditure
+        method for getting detail expenditure.
         '''
         params = req.params
         start = datetime.strptime(params['start'], '%Y-%m-%d')
@@ -144,21 +145,18 @@ class DetailExpenditure01():
         for i in response_data['records']:
             list_.append(list(i.values()))
 
-        
-            
         resp.status = falcon.HTTP_200  #pylint: disable=no-member
-        io = StringIO()
-        data = json.dump(list_, io)
-        resp.body = io.getvalue()
+        resp.body = json.dumps(list_)
 
 @falcon.before(validate_date)
 class DetailExpenditureWeek():
     '''
     detail exp
     '''
-    def on_get(self, req, resp):
+    def on_post(self, req, resp):
         '''
-        sample payload(filter Key -> Head type (Major, Minor...), fileter Value -> )
+        sample payload
+        {"filters": {"major": "2011, 2216", "sub_major": "01, 02"}}
         '''
         params = req.params
         start = datetime.strptime(params['start'], '%Y-%m-%d')
@@ -168,30 +166,33 @@ class DetailExpenditureWeek():
         if req_body:
             payload = json.loads(req_body)
         else:
-            payload = {'filters':{'major':'2011, 2216', 'sub_major': '01,02' }}
+            payload = {}
 
         if not payload:
-            query_string = "select sum(SANCTION), sum(ADDITION), sum(SAVING), sum(REVISED) from himachal_budget_allocation_expenditure WHERE date BETWEEN '{}' and '{}' group by WEEK(DATE(date))"  # pylint: disable=line-too-long
+            query_string = """
+                SELECT sum(SANCTION), sum(ADDITION), sum(SAVING), sum(REVISED)
+                FROM himachal_budget_allocation_expenditure
+                WHERE date BETWEEN '{}' and '{}'
+                GROUP BY WEEK(DATE(date))
+            """
         else:
-            select = "SELECT major, sum(SANCTION), sum(ADDITION), sum(SAVING), sum(REVISED)" 
+            select = "SELECT major, sum(SANCTION), sum(ADDITION), sum(SAVING), sum(REVISED)"
             from_str = "FROM himachal_budget_allocation_expenditure"
             where = "WHERE date BETWEEN '{}' and '{}'".format(start, end)
             groupby = "GROUP BY WEEK(DATE(date))"
-            
-            for key,value in payload['filters'].items():
+
+            for key, value in payload['filters'].items():
                 where += "AND {key} IN ({value})".format(key=key, value=value)
                 groupby += ", {key}".format(key=key)
-            
-            
-            query_string = select + ' ' + from_str + '' + where + ' ' + groupby
-        
+
+            query_string = select + ' ' + from_str + ' ' + where + ' ' + groupby
 
         query = CONNECTION.execute(query_string)
         data_rows = query.fetchall()
         records = []
         for row in data_rows:
             records.append(row.values())
-        data_response =  json.dumps({'records': records, 'count': len(records)})
-       
+        data_response = json.dumps({'records': records, 'count': len(records)})
+
         resp.status = falcon.HTTP_200  #pylint: disable=no-member
         resp.body = data_response
