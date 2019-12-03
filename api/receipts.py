4b06b730bc59cf68b5a04692410868f28767454a
shreyaagrawal0809 <shreyaagrawal0809@gmail.com>
2019-12-03 14:02:16 +0530

    add detail receipts week wise end point

---
+++ api/receipts.py
@@ -0,0 +1,84 @@
+'''
+receipts data endpoints
+'''
+import json
+from datetime import datetime
+import falcon
+from api.db import CONNECTION
+
+
+class CORSMiddleware:
+    '''
+    Middleware for handling CORS.
+    '''
+    def process_request(self, req, resp):
+        '''
+        process request to set headers.
+        '''
+        resp.set_header('Access-Control-Allow-Origin', '*')
+
+
+#NOTE: purposefully ignoring no-member errors
+# Ref https://github.com/falconry/falcon/issues/1553
+def validate_date(req, resp, resource, params):
+    '''
+    check for required parameters in query string and validate date format.
+    '''
+    params = req.params
+    if 'start' not in params or 'end' not in params:
+        resp.status = falcon.HTTP_400  #pylint: disable=no-member
+        raise falcon.HTTPBadRequest('Incomplete Request', 'start and end date is required')  #pylint: disable=no-member
+    try:
+        datetime.strptime(params['start'], '%Y-%m-%d')
+        datetime.strptime(params['end'], '%Y-%m-%d')
+    except ValueError as err:
+        resp.status = falcon.HTTP_400  #pylint: disable=no-member
+        raise falcon.HTTPBadRequest('Invalid Params', str(err))  #pylint: disable=no-member
+
+@falcon.before(validate_date)
+class DetailReceiptsWeek():
+    '''
+    detail receipts endpoint
+    '''
+    def on_post(self, req, resp):
+        '''
+        Receipts data endpoint
+        '''
+        params = req.params
+        start = datetime.strptime(params['start'], '%Y-%m-%d')
+        end = datetime.strptime(params['end'], '%Y-%m-%d')
+
+        req_body = req.stream.read()
+        if req_body:
+            payload = json.loads(req_body)
+        else:
+            payload = {}
+
+        if not payload:
+            query_string = """
+                SELECT sum(Total_Receipt)
+                FROM himachal_budget_receipts_data
+                WHERE date BETWEEN '{}' and '{}'
+                GROUP BY WEEK(DATE(date))
+            """
+        else:
+            select = "SELECT sum(Total_Receipt)"
+            from_str = "FROM himachal_budget_receipts_data"
+            where = "WHERE date BETWEEN '{}' and '{}'".format(start, end)
+            groupby = "GROUP BY WEEK(DATE(date))"
+
+            for key, value in payload['filters'].items():
+                where += "AND {key} IN ({value})".format(key=key, value=value)
+            query_string = select + ' ' + from_str + ' ' + where + ' ' + groupby
+
+        query = CONNECTION.execute(query_string)
+        data_rows = query.fetchall()
+        records = []
+        print(query_string)
+        for row in data_rows:
+            records.append(row.values())
+        data_response = json.dumps({'records': records, 'count': len(records)})
+
+        resp.status = falcon.HTTP_200  #pylint: disable=no-member
+        resp.body = data_response
+
