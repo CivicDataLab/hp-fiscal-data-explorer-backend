67f8202162bdc97bbf14dc9def5369cfd3e77f32
shreyaagrawal0809 <shreyaagrawal0809@gmail.com>
2019-12-05 19:18:57 +0530

    delete day wise detail expenditure endpoints

--- api/budget_ep.py
+++ api/budget_ep.py
@@ -7,47 +7,6 @@ import falcon
 from api.db import CONNECTION
 from api.utils import validate_date, CORSMiddleware
 
-@falcon.before(validate_date)
-class DetailExpenditure():
-    '''
-    detail exp
-    '''
-    def on_get(self, req, resp):
-        '''
-        method for getting detail expenditure on daily basis
-        '''
-        params = req.params
-        start = datetime.strptime(params['start'], '%Y-%m-%d')
-        end = datetime.strptime(params['end'], '%Y-%m-%d')
-        query_string = "select * from himachal_budget_allocation_expenditure WHERE date BETWEEN '{}' and '{}'"  # pylint: disable=line-too-long
-        query = CONNECTION.execute(query_string.format(start, end))
-        data_rows = query.fetchall()
-        response_data = {'records': []}
-        for row in data_rows:
-            record = {}
-            date = datetime.strftime(row[11], '%Y%m%d')
-            record['demand'] = row[1]
-            record['major'] = row[2]
-            record['sub_major'] = row[3]
-            record['minor'] = row[4]
-            record['sub_minor'] = row[5]
-            record['budget'] = row[6]
-            record['voted_charged'] = row[7]
-            record['plan_nonplan'] = row[8]
-            record['soe'] = row[9]
-            record['soe_desc'] = row[10]
-            record['date'] = date
-            record['sanction'] = row[12]
-            record['addition'] = row[13]
-            record['savings'] = row[14]
-            record['revised'] = row[15]
-
-            response_data['records'].append(record)
-
-        resp.status = falcon.HTTP_200  #pylint: disable=no-member
-        resp.body = json.dumps(response_data)
-
-
 class ExpenditureSummary():
     """Expenditure Summary"""
 
@@ -72,51 +31,6 @@ class ExpenditureSummary():
 
         resp.status = falcon.HTTP_200  #pylint: disable=no-member
         resp.body = json.dumps(response_data)
-
-
-@falcon.before(validate_date)
-class DetailExpenditure01():
-    '''
-    detail exp without keys on daily basis
-    '''
-    def on_get(self, req, resp):
-        '''
-        method for getting detail expenditure.
-        '''
-        params = req.params
-        start = datetime.strptime(params['start'], '%Y-%m-%d')
-        end = datetime.strptime(params['end'], '%Y-%m-%d')
-        query_string = "select * from himachal_budget_allocation_expenditure WHERE date BETWEEN '{}' and '{}'"  # pylint: disable=line-too-long
-        query = CONNECTION.execute(query_string.format(start, end))
-        data_rows = query.fetchall()
-
-        response_data = {'records': []}
-        for row in data_rows:
-            record = {}
-            date = datetime.strftime(row[11], '%Y%m%d')
-            record['demand'] = row[1]
-            record['major'] = row[2]
-            record['sub_major'] = row[3]
-            record['minor'] = row[4]
-            record['sub_minor'] = row[5]
-            record['budget'] = row[6]
-            record['voted_charged'] = row[7]
-            record['plan_nonplan'] = row[8]
-            record['soe'] = row[9]
-            record['date'] = date
-            record['sanction'] = row[12]
-            record['addition'] = row[13]
-            record['savings'] = row[14]
-            record['revised'] = row[15]
-
-            response_data['records'].append(record)
-        list_ = []
-
-        for i in response_data['records']:
-            list_.append(list(i.values()))
-
-        resp.status = falcon.HTTP_200  #pylint: disable=no-member
-        resp.body = json.dumps(list_)
 
 @falcon.before(validate_date)
 class DetailExpenditureWeek():
