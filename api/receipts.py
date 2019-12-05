0a3f66c650eb696bebd91916829669d303c81989
shreyaagrawal0809 <shreyaagrawal0809@gmail.com>
2019-12-05 19:11:34 +0530

    add detail receipts month wise endpoint

--- api/receipts.py
+++ api/receipts.py
@@ -55,6 +55,53 @@ class DetailReceiptsWeek():
         resp.status = falcon.HTTP_200  #pylint: disable=no-member
         resp.body = data_response
 
+@falcon.before(validate_date)
+class DetailReceiptsMonth():
+    '''
+    detail receipts endpoint
+    '''
+    def on_post(self, req, resp):
+        '''
+        Receipts data endpoint
+        '''
+        params = req.params
+        start = datetime.strptime(params['start'], '%Y-%m-%d')
+        end = datetime.strptime(params['end'], '%Y-%m-%d')
+
+        req_body = req.stream.read()
+        if req_body:
+            payload = json.loads(req_body)
+        else:
+            payload = {}
+
+        if not payload:
+            query_string = """
+                SELECT sum(Total_Receipt)
+                FROM himachal_budget_receipts_data
+                WHERE date BETWEEN '{}' and '{}'
+                GROUP BY MONTH(DATE(date))
+            """
+        else:
+            select = "SELECT sum(Total_Receipt)"
+            from_str = "FROM himachal_budget_receipts_data"
+            where = "WHERE date BETWEEN '{}' and '{}'".format(start, end)
+            groupby = "GROUP BY MONTH(DATE(date))"
+
+            for key, value in payload['filters'].items():
+                where += "AND {key} IN ({value})".format(key=key, value=value)
+            query_string = select + ' ' + from_str + ' ' + where + ' ' + groupby
+
+        query = CONNECTION.execute(query_string)
+        data_rows = query.fetchall()
+        records = []
+        print(query_string)
+        for row in data_rows:
+            records.append(row.values())
+        data_response = json.dumps({'records': records, 'count': len(records)})
+
+        resp.status = falcon.HTTP_200  #pylint: disable=no-member
+        resp.body = data_response
+
 class ReceiptsAccountHeads():
     '''
     This API will give permutations and combinations of all account heads
