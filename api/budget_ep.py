081fc3f3a8d8eefb3818e8f8b33497a118836553
shreyaagrawal0809 <shreyaagrawal0809@gmail.com>
2020-01-09 16:20:52 +0530

    add filters API for district wise spending

--- api/budget_ep.py
+++ api/budget_ep.py
@@ -360,11 +360,84 @@ class TreasuryAccountHeads():
         '''
         Method for getting Permutations Combinations of account heads
         '''
-        query_string = "select District, Treasury_Code, Treasury, DDO_Code, DDO_Desc,demand,demand_desc,major,major_desc,sub_major,sub_major_desc,minor,minor_desc,sub_minor,sub_major_desc, budget, voted_charged, plan_nonplan, SOE, SOE_description from himachal_pradesh_district_spending_data GROUP BY District, Treasury_Code, Treasury, DDO_Code, DDO_Desc,demand,demand_desc,major,major_desc,sub_major,sub_major_desc,minor,minor_desc,sub_minor,sub_major_desc, budget, voted_charged, plan_nonplan, SOE, SOE_description"  # pylint: disable=line-too-long
+        params = req.params
+        list_of_keys = [ k for k in params.keys() ]
+        select = "SELECT District,Treasury_Code,Treasury, DDO_Code, DDO_Desc, demand, demand_desc, major, major_desc, sub_major, sub_major_desc, minor, minor_desc, sub_minor, sub_minor_desc,budget, voted_charged, plan_nonplan, SOE, SOE_description"
+        from_str = " FROM himachal_pradesh_district_spending_data_desc"
+        where = "where "
+        groupby = "GROUP BY District, Treasury_Code,Treasury, DDO_Code, DDO_Desc,demand, demand_desc, major, major_desc, sub_major, sub_major_desc, minor, minor_desc, sub_minor, sub_minor_desc,budget, voted_charged,plan_nonplan, SOE, SOE_description"
+        
+        for key, value in params.items():
+            where += "{key} IN ({value}) AND ".format(key=key, value=value)
+        query_string = select + ' ' + from_str + ' ' + where[:len(where)-4] + ' ' + groupby
+        print(query_string)
+
         query = CONNECTION.execute(query_string)
         data_rows = query.fetchall()
         records = []
-        for row in data_rows:
-            records.append(row.values())
+
+        records = [row.values() for row in data_rows]     
+        records_with_desc = [] 
+
+        for i in range(len(records)):
+            records_list = []
+            records_list.append(records[i][0:1])
+            for j in range(1,15,2):
+                records_list.append(['-'.join(records[i][j:j+2])])
+            records_list.append(records[i][15:18])
+            records_list.append(['-'.join(records[i][18:20])])
+            records_list = [acc_heads for acc_heads_value in records_list for acc_heads in acc_heads_value]
+            records_with_desc.append(records_list)
+           
+
+        dict_hp = {}
+
+        for rows in records_with_desc:
+            current_level = dict_hp
+            for acc_heads in rows:
+                if acc_heads not in current_level:
+                    current_level[acc_heads] = {}
+                current_level = current_level[acc_heads]
+        
         resp.status = falcon.HTTP_200  #pylint: disable=no-member
-        resp.body = json.dumps({'records':records, 'count': len(records)})
+        resp.body = json.dumps({'records':dict_hp})  
+
+class UniqueAccountHeadsTreasury():
+    '''
+    This API will give permutations and combinations of all account heads
+    '''
+    def on_get(self, req, resp):
+        '''
+        Method for getting Permutations Combinations of account heads
+        '''
+        query_string = "SELECT `COLUMN_NAME`  FROM `INFORMATION_SCHEMA`.`COLUMNS`  WHERE `TABLE_SCHEMA`='himachal_pradesh_data' AND `TABLE_NAME`='himachal_pradesh_district_spending_data_desc'"  # pylint: disable=line-too-long
+        get_column_names = CONNECTION.execute(query_string)
+        column_names = get_column_names.fetchall()
+        
+        column_names_list  =  [row.values() for row in column_names]     
+        
+        list_acc_heads_with_desc =column_names_list[2:16] + column_names_list[19:21]
+        
+        list_acc_heads_without_desc = column_names_list[1:2] + column_names_list[16:19]
+        list_acc_heads_without_desc = [acc_heads for acc_heads_value in list_acc_heads_without_desc  for acc_heads in acc_heads_value]
+        dict_unique_acc_heads = {}
+        #pdb.set_trace()
+    
+        for acc_heads_index in range(0,15,2):
+            query_select = "select distinct concat_ws('-',{},{}) from himachal_pradesh_district_spending_data_desc".format(list_acc_heads_with_desc[acc_heads_index][0],list_acc_heads_with_desc[acc_heads_index+1][0])
+            query_unique_acc_heads = CONNECTION.execute(query_select)
+            unique_acc_heads_value = query_unique_acc_heads.fetchall()
+            unique_acc_heads_value_list =  [row_acc.values() for row_acc in unique_acc_heads_value] 
+            unique_acc_heads_value_list = [acc_heads for acc_heads_value in unique_acc_heads_value_list for acc_heads in acc_heads_value]
+            dict_unique_acc_heads[list_acc_heads_with_desc[acc_heads_index][0]] = unique_acc_heads_value_list
+
+        for acc_heads_index in range(0,4):
+            query_select = "select distinct {} from himachal_pradesh_district_spending_data_desc".format(list_acc_heads_without_desc[acc_heads_index])
+            query_unique_acc_heads = CONNECTION.execute(query_select)
+            unique_acc_heads_value = query_unique_acc_heads.fetchall()
+            unique_acc_heads_value_list =  [row_acc.values() for row_acc in unique_acc_heads_value] 
+            unique_acc_heads_value_list = [acc_heads for acc_heads_value in unique_acc_heads_value_list for acc_heads in acc_heads_value]
+            dict_unique_acc_heads[list_acc_heads_without_desc[acc_heads_index]] = unique_acc_heads_value_list
+
+        resp.status = falcon.HTTP_200  #pylint: disable=no-member
+        resp.body = json.dumps(dict_unique_acc_heads)
