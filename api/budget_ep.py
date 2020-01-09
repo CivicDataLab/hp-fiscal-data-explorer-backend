5d9810e95e6bc8ceab4f84e83f9b59a85e9b45f2
shreyaagrawal0809 <shreyaagrawal0809@gmail.com>
2020-01-09 13:11:17 +0530

    change API structure with keys as districts

--- api/budget_ep.py
+++ api/budget_ep.py
@@ -132,7 +132,7 @@ class DetailExpenditureMonth():
         resp.body = data_response
 
 @falcon.before(validate_date)
-@falcon.before(validate_vis_type)
+@falcon.before(validate_vis_range)
 class TreasuryExpenditureVisType():
     '''
     detail exp
@@ -145,7 +145,7 @@ class TreasuryExpenditureVisType():
         params = req.params
         start = datetime.strptime(params['start'], '%Y-%m-%d')
         end = datetime.strptime(params['end'], '%Y-%m-%d')
-        vis_type = params['type']
+        vis_range = params['range']
 
         req_body = req.stream.read()
         if req_body:
@@ -153,38 +153,41 @@ class TreasuryExpenditureVisType():
         else:
             payload = {}
 
-        # if not payload:
-        #     query_string = """
-        #         SELECT sum(GROSS), sum(AGDED), sum(BTDED), sum(NETPAYMENT)
-        #         FROM himachal_pradesh_district_spending_data
-        #         WHERE date BETWEEN '{}' and '{}'
-        #         GROUP BY MONTH(DATE(TRANSDATE))
-        #     """
-        # else:
-
-        if vis_type =='District':
-            select = "SELECT District,sum(GROSS), sum(AGDED), sum(BTDED), sum(NETPAYMENT)"
-            from_str = "FROM himachal_pradesh_district_spending_data"
-            where = "WHERE TRANSDATE BETWEEN '{}' and '{}'".format(start, end)
-            groupby = "GROUP BY District"
-        else:
-            select = "SELECT sum(GROSS), sum(AGDED), sum(BTDED), sum(NETPAYMENT)"
-            from_str = "FROM himachal_pradesh_district_spending_data"
-            where = "WHERE TRANSDATE BETWEEN '{}' and '{}'".format(start, end)
-            groupby = "GROUP BY {}(DATE(TRANSDATE))".format(vis_type)
+    
+        select = "SELECT district,sum(GROSS), sum(AGDED), sum(BTDED), sum(NETPAYMENT)"
+        from_str = "FROM himachal_pradesh_district_spending_data_desc"
+        where = "WHERE TRANSDATE BETWEEN '{}' and '{}'".format(start, end)
+        groupby = "GROUP BY district, {}(DATE(TRANSDATE))".format(vis_range)
 
 
         for key, value in payload['filters'].items():
             where += "AND {key} IN ('{value}')".format(key=key, value=value)
         query_string = select + ' ' + from_str + ' ' + where + ' ' + groupby
-       
+      
         query = CONNECTION.execute(query_string)
         data_rows = query.fetchall()
         records = []
-        print(query_string)
+        
         for row in data_rows:
             records.append(row.values())
-        data_response = json.dumps({'records': records, 'count': len(records)})
+        dict_hp = {}
+
+        districts = []
+        values = []
+
+        for i in records:
+            districts.append(i[0])
+            values.append(i[1:])
+
+        dict_hp = {}
+
+        for i in districts:
+            dict_hp[i] = []
+
+        for i in range(0,len(districts)):
+            dict_hp[districts[i]].append(values[i])
+
+        data_response = json.dumps({'records': dict_hp, 'count': len(records)})
 
         resp.status = falcon.HTTP_200  #pylint: disable=no-member
         resp.body = data_response
@@ -212,13 +215,13 @@ class TreasuryExpenditureWeek():
         if not payload:
             query_string = """
                 SELECT sum(GROSS), sum(AGDED), sum(BTDED), sum(NETPAYMENT)
-                FROM himachal_pradesh_district_spending_data
+                FROM himachal_pradesh_district_spending_data_desc
                 WHERE date BETWEEN '{}' and '{}'
                 GROUP BY WEEK(DATE(TRANSDATE))
             """
         else:
             select = "SELECT sum(GROSS), sum(AGDED), sum(BTDED), sum(NETPAYMENT)"
-            from_str = "FROM himachal_pradesh_district_spending_data"
+            from_str = "FROM himachal_pradesh_district_spending_data_desc"
             where = "WHERE TRANSDATE BETWEEN '{}' and '{}'".format(start, end)
             groupby = "GROUP BY WEEK(DATE(TRANSDATE))"
 
