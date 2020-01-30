f3eedd2959bc999611ed4c4c042ffe2c1d248a8e
shreyaagrawal0809 <shreyaagrawal0809@gmail.com>
2020-01-30 17:22:16 +0530

    add schemes end points

--- api/app.py
+++ api/app.py
@@ -4,7 +4,7 @@ api definition
 
 import falcon
 
-from api import budget_ep, receipts
+from api import budget_ep, receipts, schemes
 
 # create API
 api = app = falcon.API(middleware=[budget_ep.CORSMiddleware(), receipts.CORSMiddleware()])
@@ -18,9 +18,12 @@ api.add_route('/api/acc_heads_test', budget_ep.DetailAccountHeadsTest())
 api.add_route('/api/acc_heads_desc', budget_ep.DetailAccountHeadsDesc())
 api.add_route('/api/unique_acc_heads', budget_ep.UniqueAccountHeads())
 api.add_route('/api/unique_acc_heads_treasury', budget_ep.UniqueAccountHeadsTreasury())
+api.add_route('/api/unique_acc_heads_schemes', schemes.UniqueAccountHeadsSchemes())
 api.add_route('/api/detail_receipts_week', receipts.DetailReceiptsWeek())
 api.add_route('/api/detail_receipts_month', receipts.DetailReceiptsMonth())
 api.add_route('/api/treasury_exp', budget_ep.TreasuryExpenditureVisType())
 api.add_route('/api/treasury_exp_week', budget_ep.TreasuryExpenditureWeek())
 api.add_route('/api/acc_heads_treasury', budget_ep.TreasuryAccountHeads())
 api.add_route('/api/acc_heads_receipts', receipts.ReceiptsAccountHeads())
+api.add_route('/api/acc_heads_schemes',schemes.SchemesAccountHeads())
+api.add_route('/api/schemes', schemes.SchemesVisType())
